bbdfb26dee98c46adeb22e0cdd04402e852da33b
nicothebrush <nicola.riolini@gmail.com>
2020-01-27 12:32:03 +0100

    add images

--- wordpress_connector/models/wordpress.py
+++ wordpress_connector/models/wordpress.py
@@ -1,11 +1,86 @@
 # Copyright 2019  Micronaet SRL (<http://www.micronaet.it>).
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-import xlsxwriter
+import os
 import logging
+import base64
+import woocommerce
 from odoo import models, fields, api
 
 _logger = logging.getLogger(__name__)
+
+
+class WPConnector(models.Model):
+    """ Model name: Wordpress Connector
+    """
+    _name = 'wp.connector'
+    _description = 'Wordpress Connector'
+    _order = 'company_id'
+
+    # -------------------------------------------------------------------------
+    # Utility:
+    # -------------------------------------------------------------------------
+    @api.model
+    def get_connector(self):
+        """ Connect with Word Press API management
+        """
+        connector = self[0]
+        _logger.info('>>> Connecting: %s%s API: %s, timeout=%s' % (
+            connector.url,
+            connector.version,
+            connector.api,
+            connector.timeout,
+            ))
+        try:
+            return woocommerce.API(
+                url=connector.url,
+                consumer_key=connector.key,
+                consumer_secret=connector.secret,
+                wp_api=connector.api,
+                version=connector.version,
+                timeout=connector.timeout,
+                )
+        except:
+            _logger.error('Cannot connect to Wordpress!!')
+
+    # -------------------------------------------------------------------------
+    #                                   COLUMNS:
+    # -------------------------------------------------------------------------
+    company_id = fields.Many2one(
+        comodel_name='res.company',
+        string='Company',
+        required=True)
+
+    url = fields.Char('WP URL', size=180, required=True)
+    key = fields.Char('WP consumer key', size=180, required=True)
+    secret = fields.Char('WP consumer secret', size=180, required=True)
+
+    api = fields.Boolean('WP API', default=True)
+    version = fields.Char(
+        'WP Version', size=10, default='wc/v3', required=True)
+
+    image_path = fields.Char('Image path', size=180)
+    timeout = fields.Integer('Timeout', default=600)
+
+    # album_ids = fields.Many2many(
+    #    'product.image.album',
+    #    'connector_album_rel', 'server_id', 'album_id', 'Album')
+
+
+class WPCategory(models.Model):
+    """ Model name: Wordpress Category
+    """
+    _name = 'wp.category'
+    _description = 'Wordpress Category'
+    _order = 'name'
+
+    # -------------------------------------------------------------------------
+    #                                   COLUMNS:
+    # -------------------------------------------------------------------------
+    name = fields.Char('Name', size=64, required=True)
+    wp_id = fields.Integer(string='Wp ID', readonly=True)
+    description = fields.Text('Description')
+    # TODO Image
 
 
 class WPAttribute(models.Model):
@@ -22,7 +97,7 @@ class WPAttribute(models.Model):
     wp_id = fields.Integer(string='Wp ID', readonly=True)
     description = fields.Text('Description')
     is_variation = fields.Boolean('Is variation')
-    # Image?
+    # TODO Image?
 
 
 class WPAttributeTerm(models.Model):
@@ -58,21 +133,46 @@ class WPAttributeRelations(models.Model):
         )
 
 
-class ProductProduct(models.Model):
-    """ Model name: Product
+class ProductTemplate(models.Model):
+    """ Model name: Product Template
     """
-    _inherit = 'product.product'
+    _inherit = 'product.template'
+
+    # -------------------------------------------------------------------------
+    #                                FIELD FUNCTION:
+    # -------------------------------------------------------------------------
+    @api.multi
+    def _get_wp_image_file(self):
+        """ Return image loading from file:
+        """
+        connector = self[0].company_id.wp_connector_ids[0]
+        path = connector.image_path
+
+        for product in self:
+            if not path:
+                product.wp_image = False
+                _logger.info('Missed path, check connector parameter')
+                continue
+
+            filename = os.path(
+                path, product.default_code.replace(' ', '&nbsp;'))
+            try:
+                f_data = open(filename, 'rb')
+                product.wp_image = base64.encodebytes(f_data.read())
+                f_data.close()
+            except:
+                product.wp_image = False
 
     # -------------------------------------------------------------------------
     #                                   COLUMNS:
     # -------------------------------------------------------------------------
     wp_master = fields.Boolean(
-        string='Wp_master', help='Wordpress master product')
+        string='Is master', help='Wordpress master product')
     wp_master_id = fields.Many2one(
-        comodel_name='product.product',
+        comodel_name='product.template',
         string='Master')
     wp_default_id = fields.Many2one(
-        comodel_name='product.product',
+        comodel_name='product.template',
         domain="[('wp_master_id', '=', active_id)]",
         string='Default')
     wp_variation_term_id = fields.Many2one(
@@ -81,16 +181,34 @@ class ProductProduct(models.Model):
         help='Term used for this variation'
         )
 
+    wp_image = fields.Binary(
+         compute=_get_wp_image_file,
+         help='Load image from folder for connector', string='WP Image')
 
-class ProductProductRelation(models.Model):
-    """ Model name: Product
+
+class ProductTemplateRelation(models.Model):
+    """ Model name: Product Template
     """
-    _inherit = 'product.product'
+    _inherit = 'product.template'
 
     # -------------------------------------------------------------------------
     #                                   COLUMNS:
     # -------------------------------------------------------------------------
     wp_slave_ids = fields.One2many(
-        comodel_name='product.product',
+        comodel_name='product.template',
         inverse_name='wp_master_id',
         string='Slave')
+
+
+class ResCompany(models.Model):
+    """ Model name: Company
+    """
+    _inherit = 'res.company'
+
+    # -------------------------------------------------------------------------
+    #                                   COLUMNS:
+    # -------------------------------------------------------------------------
+    wp_connector_ids = fields.One2many(
+        comodel_name='wp.connector',
+        inverse_name='company_id',
+        string='Connector')
