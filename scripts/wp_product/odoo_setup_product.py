2f82869e73e0e86b60ea4cc995623b01c36eb1ce
nicothebrush <nicola.riolini@gmail.com>
2020-01-28 18:35:05 +0100

    generic bugfix (fast message)

--- scripts/wp_product/odoo_setup_product.py
+++ scripts/wp_product/odoo_setup_product.py
@@ -108,8 +108,8 @@ for default_code in check_product:
         if not supplier:
             continue
         supplier_id = supplier_db.get(supplier, False)
-        if not supplier_id:
-            not_found.append(supplier_id)
+        if not supplier_id and supplier_id not in not_found:
+            not_found.append(supplier)
             print 'Not found supplier code: %s' % supplier
             continue
             
