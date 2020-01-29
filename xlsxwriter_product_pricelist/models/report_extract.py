fb6cffb489865ad1180abf1466412a76d608d95d
nicothebrush <nicola.riolini@gmail.com>
2020-01-29 14:44:15 +0100

    add internal report

--- xlsxwriter_product_pricelist/models/report_extract.py
+++ xlsxwriter_product_pricelist/models/report_extract.py
@@ -12,12 +12,109 @@ class ProductProductExcelReportWizard(models.TransientModel):
     _description = 'Extract product report'
 
     supplier_id = fields.Many2one('res.partner', 'Supplier')
+    mode = fields.Selection(
+        string='Mode',
+        selection=[
+            ('supplier', 'Supplier per page'),
+            ('internal', 'Internal'),
+            ],
+        required=True, default='supplier')
 
     @api.multi
     def excel_product_report(self, ):
+        """ Button event, call correct mode
+        """
+        if self.mode == 'supplier':
+            return self.excel_product_report_supplier_mode()
+        else:
+            return self.excel_product_report_internal_mode()
+
+    @api.multi
+    def excel_product_report_internal_mode(self, ):
+        """ Mode: Internal
+        """
+        report_pool = self.env['excel.report']
+        product_pool = self.env['product.template']
+
+        # Excel file configuration:
+        header = (
+            'Ref.', 'Nome', 'Codice',
+            'Categoria', 'Listino',
+            )
+        column_width = (
+            5, 55, 15,
+            20, 10,
+            )
+        from_col = len(header)
+
+        ws_name = 'Prodotti'
+        report_pool.create_worksheet(ws_name, format_code='DEFAULT')
+        report_pool.column_width(ws_name, column_width)
+        report_pool.column_hidden(ws_name, [0])  # Hide 1st column
+
+        # Title:
+        row = 0
+        title = ('Elenco prodotti con fornitori abbinati', )
+        report_pool.write_xls_line(
+            ws_name, row, title, style_code='title')
+
+        # Header:
+        row += 1
+        report_pool.write_xls_line(
+            ws_name, row, header, style_code='header')
+
+        max_level = 0
+        for product in product_pool.search([]):
+            row += 1
+            report_pool.write_xls_line(ws_name, row, (
+                product.id,
+                product.name,
+                product.default_code or '',
+
+                product.categ_id.name or '',
+                (product.list_price, 'number'),
+                ('', 'number'),
+            ), style_code='text')
+
+            supplier_block = []
+            supplier_total = len(product.seller_ids)
+            if supplier_total > max_level:
+                max_level = supplier_total
+
+            for supplier in product.seller_ids:
+                supplier_block.extend([
+                    supplier.name.name,
+                    (supplier.price, 'number')])
+
+            report_pool.write_xls_line(
+                ws_name, row, supplier_block, style_code='text', col=from_col)
+
+        # ---------------------------------------------------------------------
+        # Setup columns for extra header:
+        # ---------------------------------------------------------------------
+        row = 1
+        for i in range(max_level):
+            header_col = from_col + 2 * i
+            report_pool.column_width(ws_name, (30, 8), col=header_col)
+            report_pool.write_xls_line(
+                ws_name, row,
+                ('Fornitore %s' % (i + 1), 'Costo %s' % (i + 1)),
+                style_code='header', col=header_col)
+
+        # ---------------------------------------------------------------------
+        # Return file:
+        # ---------------------------------------------------------------------
+
+        return report_pool.return_attachment('Report_Product')
+
+    @api.multi
+    def excel_product_report_supplier_mode(self, ):
+        """ Mode: Supplier
+        """
         report_pool = self.env['excel.report']
         detail_pool = self.env['product.supplierinfo']
 
+        # Wizard parameters:
         supplier = self.supplier_id
 
         # Collect data:
@@ -93,5 +190,5 @@ class ProductProductExcelReportWizard(models.TransientModel):
                     ('', 'number'),
                     ), style_code='text')
 
-        # Save file:
+        # Return file:
         return report_pool.return_attachment('Report_Product')
