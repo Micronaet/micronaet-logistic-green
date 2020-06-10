5c327da4a79f1b80fddb6b6143245a608908254d
nicothebrush <nicola.riolini@gmail.com>
2020-06-10 10:16:58 +0200

    add procedure to clean order

---
+++ wordpress_order/script/clean_old_order/pulizia.py
@@ -0,0 +1,56 @@
+import os
+import woocommerce
+import pickle
+import urllib
+import sys
+import erppeek
+import ConfigParser
+
+# -----------------------------------------------------------------------------
+# Read configuration parameter:
+# -----------------------------------------------------------------------------
+# From config file:
+date_order = '2020-03-10'
+dry_run = True
+
+print('Clean before: %s' % date_order)
+import pdb; pdb.set_trace()
+cfg_file = os.path.expanduser('./openerp.cfg')
+
+config = ConfigParser.ConfigParser()
+config.read([cfg_file])
+dbname = config.get('dbaccess', 'dbname')
+user = config.get('dbaccess', 'user')
+pwd = config.get('dbaccess', 'pwd')
+server = config.get('dbaccess', 'server')
+port = config.get('dbaccess', 'port')   # verify if it's necessary: getint
+
+# -----------------------------------------------------------------------------
+# Connect to ODOO:
+# -----------------------------------------------------------------------------
+odoo = erppeek.Client(
+    'http://%s:%s' % (server, port),
+    db=dbname, user=user, password=pwd,
+    )
+
+# Pool used:
+order_pool = odoo.model('sale.order')
+
+order_ids = order_pool.search([
+    ('date_order', '<=', date_order),
+    ('wp_status', 'in', (
+        'delivered',
+        'sent-to-gsped',
+        # 'processing',
+        # 'on-hold',
+        # 'pending',
+    )),
+    ])
+
+print('Order to be cleaned: %s' % len(order_ids))
+for order in order_pool.browse(order_ids):
+    # Update confirmed
+    if dry_run:
+        print('Marked completed: %s' % order.name)
+    else:
+        order.wp_wf_completed()
