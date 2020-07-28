ba161cce6d95cbb6cd26c1a6aa9141964a440463
nicothebrush <nicola.riolini@gmail.com>
2020-07-28 07:50:48 +0200

    generic bugfix (fast message)

--- wordpress_order_carrier/models/order_carrier.py
+++ wordpress_order_carrier/models/order_carrier.py
@@ -67,6 +67,7 @@ class SaleOrder(models.Model):
         if not error:
             # Update SOAP data for real call
             tracking_status = reply['TrackingStatus']
+            pdb.set_trace()
             if tracking_status != order.delivery_soap_state:
                 pdb.set_trace()  # TODO check for update wordpress status
                 # -------------------------------------------------------------
