4ce61404a6d8c01480b80bd841b07ab9266159d9
nicothebrush <nicola.riolini@gmail.com>
2020-08-12 15:00:33 +0200

    add also moved image procedure

--- wordpress_order/script/product_change_wp_manage/change_wp_ref.py
+++ wordpress_order/script/product_change_wp_manage/change_wp_ref.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import shutil
+import pdb
 from datetime import datetime, timedelta
 import erppeek
 import ConfigParser
@@ -7,6 +9,10 @@ import ConfigParser
 # -----------------------------------------------------------------------------
 # Read configuration parameter:
 # -----------------------------------------------------------------------------
+old_path = '/home/odoo/.local/share/Odoo/filestore/LeGeorgiche/Data/images'
+new_path = '/home/odoo/.local/share/Odoo/filestore/LeGeorgiche/Data/wp_images'
+extension = 'jpg'
+
 # From config file:
 cfg_file = os.path.expanduser('../openerp.cfg')
 
@@ -33,8 +39,9 @@ template_ids = template_pool.search([])
 if not template_ids:
     print('Error no product found')
     sys.exit()
-
+pdb.set_trace()
 for template in template_pool.browse(template_ids):
+    # Mode WP reference:
     default_code = template.default_code  # wp_sku
     wp_id = template.wp_id
     template_pool.write({
@@ -42,3 +49,17 @@ for template in template_pool.browse(template_ids):
         'sku_in': default_code
     })
     print('Update: %s' % default_code)
+
+    # Move image:
+    product_id = template.id
+    old_name = '%s.000.%s' % (wp_id, extension)
+    new_name = '%s.000.%s' % (product_id, extension)
+
+    try:
+        shutil.copy(
+            os.path.join(old_path, old_name),
+            os.path.join(new_path, new_name),
+        )
+        print('Copied image: %s >> %s' % (old_name, new_name))
+    except:
+        print('Error copy image: %s >> %s' % (old_name, new_name))
